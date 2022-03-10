156efb3126031ffe7e617c7fc2828b61269a53de
nmolina95 <ignaciomolina.95@hotmail.com>
2022-03-10 18:17:33 -0300

    Creado proyecto base

---
+++ src/main/java/com/mindhub/duodanzaclub/DuodanzaclubApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.duodanzaclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuodanzaclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuodanzaclubApplication.class, args);
+	}
+
+}
