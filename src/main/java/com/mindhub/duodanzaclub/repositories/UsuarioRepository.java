305f73b9a0c5b53c074fa93aeede6d3d0570b6ae
nmolina95 <ignaciomolina.95@hotmail.com>
2022-03-11 17:14:05 -0300

    Se configuró la seguridad

--- src/main/java/com/mindhub/duodanzaclub/repositories/UsuarioRepository.java
+++ src/main/java/com/mindhub/duodanzaclub/repositories/UsuarioRepository.java
@@ -2,8 +2,10 @@ package com.mindhub.duodanzaclub.repositories;
 
 import com.mindhub.duodanzaclub.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    public Usuario findByEmail(@Param("email") String email);
 }
